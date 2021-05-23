b7ccdd3c1bbb018bb13c6ba7107e9ca20225f542
Tiffany <Owhyze1@gmail.com>
2021-05-23 18:23:55 -0400

    testing implementation for once function from underscore.js

--- testingOnce.js
+++ testingOnce.js
@@ -1,23 +1,29 @@
  once = function(func) {
-  // TIP: These variables are stored in a "closure scope" (worth researching),
-  // so that they'll remain available to the newly-generated function every
-  // time it's called.
+
   var alreadyCalled = false;
   var result;
 
-  // TIP: We'll return a new function that delegates to the old one, but only
-  // if it hasn't been called before.
   return function() {
     if (!alreadyCalled) {
-      // TIP: .apply(this, arguments) is the standard way to pass on all of the
-      // infromation from one function call to another.
+
       result = func.apply(this, arguments);
       alreadyCalled = true;
     }
-    // The new function always returns the originally computed result.
     return result;
   };
 };
+
+memoize = function(func) {
+  var alreadyCalled = false;
+  var result = [];
+
+  return function() {
+    if (!alreadyCalled) {
+      alreadyCalled = true;
+      if (result.includes)
+    }
+  }
+}
 
 
 var testfunction = once(function(a, b) {
